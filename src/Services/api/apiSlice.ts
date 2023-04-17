d85fa67311d5417933ad34cbb4d74f96ec8533e2
rjcc <ryanjaycayabyab@gmail.com>
2023-04-17 08:11:27 +0800

    update apiSlice

--- src/Services/api/apiSlice.ts
+++ src/Services/api/apiSlice.ts
@@ -1,22 +1,22 @@
+// Importing necessary modules
 import {
     BaseQueryFn,
     FetchArgs,
     createApi,
     fetchBaseQuery,
     FetchBaseQueryError,
-    buildCreateApi,
-    coreModule,
-    reactHooksModule
   } from '@reduxjs/toolkit/query/react'
   import { Mutex } from 'async-mutex'
-// import { setCredentials, logOut } from './Auth/Redux/authSlice'
+  import { logOut } from "@/Services/api/Redux/authSlice";
 import type { RootState } from '../utils/store'
-import { Config } from '@/Config'
 import { setCredentials } from './Redux/authSlice'
- 
+
+// Creating a mutex object to handle asynchronous operations
 const mutex = new Mutex()
+
+// Setting up the base query with credentials and timeout
 const baseQuery = fetchBaseQuery({
-    baseUrl: Config.API_URL,
+    baseUrl: 'http://localhost:8000',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         
@@ -25,58 +25,54 @@ const baseQuery = fetchBaseQuery({
             headers.set("authorization", `Bearer ${token}`)
         }
         return headers
-    }
+    },
+    timeout: 30000
 })
 
+// Function to handle reauthentication when access token is expired
 const baseQueryWithReauth:  BaseQueryFn<
 string | FetchArgs,
 unknown,
 FetchBaseQueryError
 > = async (args, api:any, extraOptions) => {
-    await mutex.waitForUnlock()
-    let result = await baseQuery(args, api, extraOptions)
+    let result;
+    if(!mutex.isLocked()){
+        const release = await mutex.acquire()
+        try{
+            // Executing the base query
+            result = await baseQuery(args, api, extraOptions)
 
-    if (result?.error && result?.error?.status === 403) {
-        if(!mutex.isLocked()){
-            const release = await mutex.acquire()
-            try{
+            // Checking for 403 status code
+            if (result?.error && result?.error?.status === 403) {
+                // Refreshing the access token
                 const refreshResult = await baseQuery('/api/login_check', api, extraOptions)
               
                 if (refreshResult?.data) {
                     console.log('sending refresh token')
                     const user = api.getState().auth.user
-                    // store the new token 
+                    console.log(user)
+                    // Storing the new token 
                     api.dispatch(setCredentials({ ...refreshResult.data, user }))
-                    // retry the original query with new access token 
+                    // Retrying the original query with new access token 
                     result = await baseQuery(args, api, extraOptions)
                 } else {
-                    // api.dispatch(logOut())
+                    api.dispatch(logOut())
                 }
-            } finally{
-                release()
             }
-        }else{
-            await mutex.waitForUnlock()
-            result = await baseQuery(args, api, extraOptions)
+        } finally{
+            release()
         }
-
-        // send refresh token to get new access token 
-        
-        // console.log(refreshResult)
-       
+    }else{
+        await mutex.waitForUnlock()
+        result = await baseQuery(args, api, extraOptions)
     }
 
     return result
 }
 
+// Exporting the api slice
 export const apiSlice = createApi({
     reducerPath:'api',
     baseQuery: baseQueryWithReauth,
     endpoints: () => ({})
 })
-
-// export const apiSliceServerSide = buildCreateApi(
-//     coreModule(),
-//     reactHooksModule({unstable__sideEffectsInRender: true})
-//     )
-
